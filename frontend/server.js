f8029684af75dce32157c3e275187c63f846b9e0
FardeenI <fi43@njit.edu>
2026-04-08 20:19:28 -0400

    small fix

--- frontend/server.js
+++ frontend/server.js
@@ -1,17 +1,15 @@
-const path = require('path');
 const express = require('express');
-const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const distDir = path.join(__dirname, 'dist');
-const publicDir = path.join(__dirname, 'public');
-
-if (fs.existsSync(distDir)) {
-	app.use(express.static(distDir));
-	app.get('*', (req, res) => res.sendFile(path.join(distDir, 'index.html')));
-} else {
-	app.use(express.static(publicDir));
-}
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Calculator frontend serving at http://localhost:${PORT}`));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Frontend server running on port ${PORT}`);
+});
